10dfa6b697683d338a4ecf032293fe282ce0e8fa
andrey <a@>
2023-12-08 00:22:29 +0100

    Day 7_1 refactor

--- src/main/kotlin/D7_1.kt
+++ src/main/kotlin/D7_1.kt
@@ -3,8 +3,6 @@ object D7_1 {
     @JvmStatic
     fun main(args: Array<String>) {
         val input = readInput("07/sample.txt")
-        println(input)
-        println()
         val answer = solve(input)
         println(answer)
         val expected = 6440
@@ -13,6 +11,8 @@ object D7_1 {
         val result = solve(readInput("07/input.txt"))
         println("===================")
         println(result)
+        val expected2 = 253933213
+        check(result == expected2) { "Expected: $expected2, Actual: $result" }
     }
 
     private fun solve(input: String): Any {
@@ -20,52 +20,31 @@ object D7_1 {
             .map {
                 val (cards, bid) = it.split(" ")
                 Hand(cards, bid.toInt())
-            }.onEach { println("${it.strength}, ${it.cardsStrength}") }
-            .sortedWith { e1: Hand, e2: Hand ->
-                if (e1.strength == e2.strength)
-                    e1.cardsStrength.zip(e2.cardsStrength)
-                        .find { it.first != it.second }
-                        .let { if (it != null) it.first - it.second else 0}
-                else e1.strength - e2.strength
-            }.onEach { println(it) }
-            .mapIndexed { index, hand -> index + 1 to hand.bid }
-            .fold(0) {acc, (rank, bid) -> acc + rank * bid }
+            }
+            .sortedWith(compareBy({ it.strength }, { it.cardsStrength }))
+            .foldIndexed(0) {index, acc, hand -> acc + (index + 1) * hand.bid }
     }
     data class Hand(val cards:String, val bid: Int) {
         val strength = cards.groupBy { it }.map { it.value.count() }.sortedDescending()
-            .also { println(it) }
             .let {
-            if (it[0] == 5) 7//five of a kind
-            else if (it[0] == 4) 6//four of a kind
-            else if (it[0] == 3 && it[1] == 2) 5//full house
-            else if (it[0] == 3) 4// three of a kind
-            else if (it[0] == 2 && it[1] == 2) 3//two pairs
-            else if (it[0] == 2) 2 //one pair
-            else if (it[0] == 1) 1//high card
-            else error("Unrecognised hand: $cards $it")
-
-
+                when {
+                    it[0] == 5 -> 7 //five of a kind
+                    it[0] == 4 -> 6 //four of a kind
+                    it[0] == 3 && it[1] == 2 -> 5//full house
+                    it[0] == 3 -> 4// three of a kind
+                    it[0] == 2 && it[1] == 2 -> 3//two pairs
+                    it[0] == 2 -> 2 //one pair
+                    it[0] == 1 -> 1//high card
+                    else -> error("Unrecognised hand: $cards $it")
+                }
         }
         val cardsStrength = cards.map{
             cardToStrength[it] ?: error("No strength for ${cards.first()}")
-        }
+        }.toString()
     }
 
-    val cardToStrength = mapOf(
-        'A' to 14,
-        'K' to 13,
-        'Q' to 12,
-        'J' to 11,
-        'T' to 10,
-        '9' to 9,
-        '8' to 8,
-        '7' to 7,
-        '6' to 6,
-        '5' to 5,
-        '4' to 4,
-        '3' to 3,
-        '2' to 2,
-    )
+    val cardToStrength = "AKQJT98765432".reversed()
+        .zip("abcdefghijklm").toMap()
 }
 
 
