4196c87bf838ee593eee680e18f21f446f04cf48
Andrei Balakirev <balakirev.pflb@gmail.com>
2023-12-22 23:13:32 +0100

    Day 21_1 solved

---
+++ src/main/kotlin/D21_1.kt
@@ -0,0 +1,67 @@
+import java.util.PriorityQueue
+import java.util.Stack
+import kotlin.time.measureTime
+
+object D21_1 {
+
+    private const val DAY = 21
+
+    private fun checkSolution(answer: Any, expected: Any?) {
+        val measureTime = measureTime {
+            println(answer)
+            if (expected != null)
+                check(answer.toString() == expected.toString()) { "Expected: $expected, Actual: $answer" }
+        }
+        println("Time taken: $measureTime")
+        println("===================")
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        checkSolution(solve(readInput("$DAY/sample.txt"), 6), 16)
+        checkSolution(solve(readInput("$DAY/input.txt"), 64), 3788)
+    }
+
+    private fun solve(input: String, stepsCount: Int): Any {
+        val map = input.split("\n").map { it.toList() }
+        val startRow = map.indexOfFirst { it.contains('S') }
+        val startCol = map[startRow].indexOf('S')
+        val startPos = Pos(startRow, startCol)
+        val weightMap = map.map { row ->
+            row.map {
+                when (it) {
+                    '.' -> Int.MAX_VALUE
+                    '#' -> -1
+                    'S' -> Int.MAX_VALUE
+                    else -> error("Unknown symbol: $it")
+                }
+            }.toMutableList()
+        }.toMutableList()
+
+
+        val stack = Stack<Pair<Pos, Int>>()
+        stack.push(startPos to 0)
+
+        while (stack.size > 0) {
+            val (pos, distance) = stack.pop()
+            if (distance < weightMap[pos.row][pos.col]) {
+                weightMap[pos.row][pos.col] = distance
+                Dir.entries.forEach {
+                    val newPos = pos + it.delta
+                    if (newPos.row in weightMap.indices && newPos.col in weightMap[0].indices)
+                        stack.push(newPos to distance + 1)
+                }
+            }
+        }
+
+        val oddity = stepsCount % 2
+
+        return weightMap.sumOf { row ->
+            row.count {
+                it >= 0 && it % 2 == oddity && it <= stepsCount
+            }
+        }
+    }
+
+
+}
