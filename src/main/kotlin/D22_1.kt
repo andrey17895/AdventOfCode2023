f6daab749b5f659c41346234b32a366b5f80ec85
Andrei Balakirev <balakirev.pflb@gmail.com>
2023-12-23 18:40:22 +0100

    Day 22_1 solved

---
+++ src/main/kotlin/D22_1.kt
@@ -0,0 +1,46 @@
+import java.util.PriorityQueue
+import kotlin.time.measureTime
+
+object D22_1 {
+
+    private const val DAY = 22
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        checkSolution({ solve(readInput("$DAY/sample.txt")) }, 94)
+        checkSolution({ solve(readInput("$DAY/input.txt")) }, null)
+    }
+
+    private fun solve(input: String): Any {
+        val map = input.split("\n").map { it.toList() }
+
+        val startPos = Pos(0, map[0].indexOf('.'))
+        val endPos = Pos(map.size - 1, map[map.size - 1].indexOf('.'))
+
+        return findRoute(startPos, 0, mutableSetOf(), endPos, map)!!
+    }
+
+    private fun findRoute(pos: Pos, distance: Int, visited: MutableSet<Pos>, endPos: Pos, map: List<List<Char>>): Int? {
+        if (pos == endPos) return distance
+        val newVisited = visited.toMutableSet()
+        newVisited.add(pos)
+        return possibleMoves(pos, map).filter { !visited.contains(it) }.mapNotNull {
+            findRoute(it, distance + 1, newVisited, endPos, map)
+        }.maxOrNull()
+    }
+
+    // ^, >, v, and <
+
+    private fun possibleMoves(pos: Pos, map: List<List<Char>>): List<Pos> {
+        val tile = map[pos.row][pos.col]
+        return when (tile) {
+            '.' -> Dir.entries.map { pos + it.delta }
+            '^' -> listOf(pos + Dir.UP.delta)
+            '>' -> listOf(pos + Dir.RIGHT.delta)
+            'v' -> listOf(pos + Dir.DOWN.delta)
+            '<' -> listOf(pos + Dir.LEFT.delta)
+            else -> error("Unknown symbol $tile")
+        }.filter { it.row in map.indices && it.col in map[0].indices && map[it.row][it.col] != '#' }
+    }
+
+}
--- src/main/kotlin/common.kt
+++ src/main/kotlin/common.kt
@@ -1,3 +1,4 @@
+import kotlin.time.measureTime
 
 fun readInput(res: String): String =
     object {}.javaClass.getResourceAsStream(res)?.bufferedReader()?.readText().toString()
@@ -19,4 +20,18 @@ enum class Dir(val row: Int, val col: Int) {
 
 data class Pos(val row: Int, val col: Int) {
     operator fun plus(other: Pos): Pos = Pos(this.row + other.row, this.col + other.col)
+}
+
+fun checkSolution(solution: () -> Any, expected: Any?) {
+    val duration = measureTime {
+        val answer = solution()
+        if (expected != null) {
+            check(answer.toString() == expected.toString()) { "Expected: $expected, Actual: $answer" }
+            println("Right answer: $answer")
+        } else {
+            println("Answer: $answer")
+        }
+    }
+    println("Time taken: $duration")
+    println("===================")
 }
